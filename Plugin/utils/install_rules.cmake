b876b40b9720eab68acef393476e09dbbb7bca54
Abdullah <abdullah.ajeebi19@gmail.com>
2024-09-22 16:37:11 +0300

    e

--- Plugin/utils/install_rules.cmake
+++ Plugin/utils/install_rules.cmake
@@ -19,6 +19,6 @@ if(${IS_EFFECT} AND ${IS_GENERATOR})
     message("CMAKE FL SDK TEMPLATE: WARNING! this plugin is set both as a generator and an effect! will be installed in both directories!")
 endif()
 
-if(NOT ${IS_EFFECT} AND NOT {IS_GENERATOR})
+if(NOT ${IS_EFFECT} AND NOT ${IS_GENERATOR})
     message("CMAKE FL SDK TEMPLATE: WARNING! This plugin is not set as a generator nor an effect! No installation will be performed")
 endif()
